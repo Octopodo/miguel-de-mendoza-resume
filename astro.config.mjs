f69fc265a619c7660ff61ab2e040a248c40bec5c
Miguel de Mendoza <orson_wells40@hotmail.com>
2024-01-05 18:09:36 +0100

    Added tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://octopodo.github.io/",
   base: "/miguel-de-mendoza-resume",
+  integrations: [tailwind()]
 });
