9939e8d0480df7858a89545d356952738ffb33f1
Miguel de Mendoza <orson_wells40@hotmail.com>
2024-01-08 17:42:43 +0100

    Home Mounted

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,19 +7,11 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: colors.slate[50], // Color principal de la marca
-        secondary: colors.green[500], // Color secundario de la marca
-        accent: colors.purple[500], // Color de acento para elementos destacados
-        background: colors.gray[100], // Color de fondo principal
-        surface: colors.white, // Color de fondo para elementos de la interfaz de usuario como tarjetas, modales, etc.
-        error: colors.red[600], // Color para mensajes de error
-        warning: colors.yellow[500], // Color para mensajes de advertencia
-        success: colors.green[500], // Color para mensajes de éxito
-        info: colors.blue[300], // Color para mensajes de información
-        onPrimary: colors.white, // Color de texto y iconos cuando el fondo es 'primary'
-        onSecondary: colors.red[600], // Color de texto y iconos cuando el fondo es 'secondary'
-        onBackground: colors.gray[900], // Color de texto e iconos cuando el fondo es 'background'
-        onSurface: colors.gray[900] // Color de texto e iconos cuando el fondo es 'surface'
+        primary: '#b9b9b9', // Gris Claro
+        secondary: '#FFEE58', // Naranja Claro
+        accent: '#FF6E40', // Color de acento para elementos destacados
+        dark: '#494949', // Gris
+        darken: '#0d0d0df5' // Gris Oscuro
       },
       fontFamily: {
         primary: ['"Exo 2"', 'sans-serif'],
